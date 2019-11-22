02ac2a8a9692ffbfe58d0e7d2796bbaefa49ea41
Vedran Novakovic <venovako@venovako.eu>
2019-11-22 18:14:59 +0100

    further changes

---
+++ src/pgi.mk
@@ -0,0 +1,49 @@
+SHELL=/bin/bash
+ARCH=$(shell uname)
+ifeq ($(ARCH),Darwin)
+$(error PGI on macOS is not supported)
+endif # Darwin
+ifndef WP
+WP=8
+endif # !WP
+ifdef NDEBUG
+DEBUG=
+else # DEBUG
+DEBUG=g
+endif # ?NDEBUG
+RM=rm -rfv
+AR=ar
+ARFLAGS=rsv
+CPUFLAGS=-DUSE_PGI -DUSE_X64 -m64 -mp -Mnollvm
+FORFLAGS=$(CPUFLAGS) -i8 -Mdclchk -Mlarge_arrays -Mrecursive -Mstack_arrays
+C11FLAGS=$(CPUFLAGS) -c11
+CC=pgcc
+FC=pgfortran
+CXX=pgc++
+FPUFLAGS=-Kieee -Mfma -Mnodaz -Mnoflushz -Mnofpapprox -Mnofprelaxed
+ifdef NDEBUG
+OPTFLAGS=-O$(NDEBUG)
+OPTFFLAGS=$(OPTFLAGS)
+OPTCFLAGS=$(OPTFLAGS)
+DBGFLAGS=-DNDEBUG -Minfo
+DBGFFLAGS=$(DBGFLAGS)
+DBGCFLAGS=$(DBGFLAGS)
+FPUFFLAGS=$(FPUFLAGS)
+FPUCFLAGS=$(FPUFLAGS)
+else # DEBUG
+OPTFLAGS=-O0
+OPTFFLAGS=$(OPTFLAGS)
+OPTCFLAGS=$(OPTFLAGS)
+DBGFLAGS=-g -Minfo -Mbounds -Mchkstk -traceback
+DBGFFLAGS=$(DBGFLAGS)
+DBGCFLAGS=$(DBGFLAGS)
+FPUFFLAGS=$(FPUFLAGS)
+FPUCFLAGS=$(FPUFLAGS)
+endif # ?NDEBUG
+LIBFLAGS=-I. -I../shared -I../../../JACSD/vn
+ifeq ($(ARCH),Linux)
+LIBFLAGS += -D_GNU_SOURCE
+endif # Linux
+LDFLAGS=-L../shared -ljk$(DEBUG) -L../../../JACSD -lvn$(DEBUG) -pgf90libs -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
+FFLAGS=$(OPTFFLAGS) $(DBGFFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFFLAGS)
+CFLAGS=$(OPTCFLAGS) $(DBGCFLAGS) $(LIBFLAGS) $(C11FLAGS) $(FPUCFLAGS)
