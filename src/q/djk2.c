e10ad8105b9828632b1db9d47f2a7116d567bf9e
Vedran Novakovic <venovako@venovako.eu>
2019-12-16 21:39:12 +0100

    further changes

--- src/q/djk2.c
+++ src/q/djk2.c
@@ -1,11 +1,120 @@
 #include "djk2.h"
 
+static void qhsvd2d(extended A[static 2][2], extended U[static 2][2], fint info[static 1])
+{
+  *info = FINT_C(0);
+  A[1][0] = A[0][1] = 0.0L;
+
+  if (copysignl(1.0L, A[0][0]) == -1.0L) {
+    U[0][0] = -U[0][0];
+    U[1][0] = -U[1][0];
+    A[0][0] = -A[0][0];
+  }
+
+  if (copysignl(1.0L, A[1][1]) == -1.0L) {
+    U[0][1] = -U[0][1];
+    U[1][1] = -U[1][1];
+    A[1][1] = -A[1][1];
+  }
+}
+
+static void CA1(const extended B[static 2][2], extended A[static 2])
+{
+  const extended C[2] =
+    {
+     (fmal(B[1][0], A[1], A[0]) * B[0][0]),
+     (fmal(B[0][1], A[0], A[1]) * B[1][1])
+    };
+  A[0] = C[0];
+  A[1] = C[1];
+}
+
+static void CA2(const extended B[static 2][2], extended A[static 2][2])
+{
+  const extended C[2][2] =
+    {
+     {
+      (fmal(B[1][0], A[0][1], A[0][0]) * B[0][0]),
+      (fmal(B[0][1], A[0][0], A[0][1]) * B[1][1])
+     },
+     {
+      (fmal(B[1][0], A[1][1], A[1][0]) * B[0][0]),
+      (fmal(B[0][1], A[1][0], A[1][1]) * B[1][1])
+     }
+    };
+  A[0][0] = C[0][0];
+  A[0][1] = C[0][1];
+  A[1][0] = C[1][0];
+  A[1][1] = C[1][1];
+}
+
+static void AC(extended A[static 2][2], const extended B[static 2][2])
+{
+  const extended C[2][2] =
+    {
+     {
+      (fmal(A[1][0], B[0][1], A[0][0]) * B[0][0]),
+      (fmal(A[1][1], B[0][1], A[0][1]) * B[0][0])
+     },
+     {
+      (fmal(A[0][0], B[1][0], A[1][0]) * B[1][1]),
+      (fmal(A[0][1], B[1][0], A[1][1]) * B[1][1])
+     }
+    };
+  A[0][0] = C[0][0];
+  A[0][1] = C[0][1];
+  A[1][0] = C[1][0];
+  A[1][1] = C[1][1];
+}
+
+static void qhsvd2u(const bool h, extended A[static 2][2], extended U[static 2][2], extended Z[static 2][2], fint info[static 1])
+{
+  *info = FINT_C(0);
+
+  if (h) {
+  }
+  else {
+  }
+
+  if (*info)
+    *info = FINT_C(0);
+  else {
+  }
+
+  qhsvd2d(A, U, info);
+  *info = FINT_C(1);
+}
+
+static void qhsvd2l(extended A[static 2][2], extended U[static 2][2], extended Z[static 2][2], fint info[static 1])
+{
+  *info = FINT_C(0);
+
+  if (*info)
+    *info = FINT_C(0);
+  else {
+  }
+
+  qhsvd2d(A, U, info);
+  *info = FINT_C(1);
+}
+
+static void qhsvd2g(const bool h, extended A[static 2][2], extended U[static 2][2], extended Z[static 2][2], fint info[static 1])
+{
+}
+
+static void qhsvd2s(const fint h, extended A[static 2][2], extended U[static 2][2], extended Z[static 2][2], fint info[static 1])
+{
+}
+
 static void qhsvd2_(extended A[static 2][2], const fint J[static 2], extended U[static 2][2], extended Z[static 2][2], fint info[static 1])
 {
   if ((A[0][1] == 0.0L) && (A[1][0] == 0.0L))
-    /* A diagonal */;
-  else
-    /* A general */;
+    qhsvd2d(A, U, info);
+  else /* A general */
+    qhsvd2g((J[0] != J[1]), A, U, Z, info);
+
+  if (*info >= FINT_C(0))
+    qhsvd2s((J[0] + J[1]), A, U, Z, info);
 }
 
 void dhsvd2_(double A[static 2][2], const fint J[static 2], double U[static 2][2], double Z[static 2][2], fint info[static 1])
