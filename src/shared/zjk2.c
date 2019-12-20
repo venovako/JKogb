f6039005630c941a793193558ee389ac0044eed3
Vedran Novakovic <venovako@venovako.eu>
2019-12-20 20:32:32 +0100

    further changes

--- src/shared/zjk2.c
+++ src/shared/zjk2.c
@@ -1,5 +1,32 @@
 #include "zjk2.h"
 
+#ifndef x0
+#define x0 CMPLXL(0.0L, 0.0L)
+#else /* x0 */
+#error x0 already defined
+#endif /* ?x0 */
+
+#ifndef x1
+#define x1 CMPLXL(1.0L, 0.0L)
+#else /* x1 */
+#error x1 already defined
+#endif /* ?x1 */
+
+static inline bool zisfinite(const dcomplex z)
+{
+  return (isfinite(creal(z)) && isfinite(cimag(z)));
+}
+
+static inline bool xisreal(const xcomplex x)
+{
+  return (cimagl(x) == 0.0L);
+}
+
+static inline bool xisimag(const xcomplex x)
+{
+  return (creall(x) == 0.0L);
+}
+
 static inline xcomplex xqfma(const extended a, const xcomplex b, const xcomplex c)
 {
   return CMPLXL(fmal(a, creall(b), creall(c)), fmal(a, cimagl(b), cimagl(c)));
@@ -17,9 +44,9 @@ static inline xcomplex xxfma(const xcomplex a, const xcomplex b, const xcomplex 
 
 static inline xcomplex xfma(const xcomplex a, const xcomplex b, const xcomplex c)
 {
-  if (cimagl(a) == 0.0L)
+  if (xisreal(a))
     return xqfma(creall(a), b, c);
-  if (creall(a) == 0.0L)
+  if (xisimag(a))
     return xjfma(cimagl(a), b, c);
   return xxfma(a, b, c);
 }
@@ -110,9 +137,66 @@ static inline void AC(xcomplex A[static 2][2], const xcomplex B[static 2][2])
   xassgn2(A, C);
 }
 
-static inline int zisfinite(const dcomplex z)
+static inline void xhsvd2d(xcomplex A[static 2][2], xcomplex U[static 2][2], fint info[static 1])
 {
-  return (isfinite(creal(z)) && isfinite(cimag(z)));
+  *info = FINT_C(0);
+  A[1][0] = A[0][1] = x0;
+
+  if (xisreal(A[0][0])) {
+  }
+  else if (xisimag(A[0][0])) {
+  }
+  else {
+  }
+
+  if (xisreal(A[1][1])) {
+  }
+  else if (xisimag(A[1][1])) {
+  }
+  else {
+  }
+}
+
+static inline void xhsvd2u(const bool h, xcomplex A[static 2][2], xcomplex U[static 2][2], xcomplex Z[static 2][2], fint info[static 1])
+{
+  *info = FINT_C(0);
+}
+
+static inline void xhsvd2l(xcomplex A[static 2][2], xcomplex U[static 2][2], xcomplex Z[static 2][2], fint info[static 1])
+{
+  *info = FINT_C(0);
+}
+
+static inline void xhsvd2g(const bool h, xcomplex A[static 2][2], xcomplex U[static 2][2], xcomplex Z[static 2][2], fint info[static 1])
+{
+  *info = FINT_C(0);
+}
+
+static inline void xhsvd2s(const fint h, xcomplex A[static 2][2], xcomplex U[static 2][2], xcomplex Z[static 2][2], fint info[static 1])
+{
+  if (((h == FINT_C(2)) && (creall(A[0][0]) < creall(A[1][1]))) || ((h == FINT_C(-2)) && (creall(A[0][0]) > creall(A[1][1])))) {
+    xswp(&(U[0][0]), &(U[0][1]));
+    xswp(&(U[1][0]), &(U[1][1]));
+    xswp(&(A[0][0]), &(A[1][1]));
+    xswp(&(Z[0][0]), &(Z[1][0]));
+    xswp(&(Z[0][1]), &(Z[1][1]));
+  }
+
+  if ((U[0][0] != x1) || (U[0][1] != x0) || (U[1][0] != x0) || (U[1][1] != x1))
+    *info += FINT_C(2);
+  if ((Z[0][0] != x1) || (Z[0][1] != x0) || (Z[1][0] != x0) || (Z[1][1] != x1))
+    *info += FINT_C(4);
+}
+
+static inline void xhsvd2_(xcomplex A[static 2][2], const fint J[static 2], xcomplex U[static 2][2], xcomplex Z[static 2][2], fint info[static 1])
+{
+  if ((A[0][1] == x0) && (A[1][0] == x0))
+    xhsvd2d(A, U, info);
+  else /* A general */
+    xhsvd2g((J[0] != J[1]), A, U, Z, info);
+
+  if (*info >= FINT_C(0))
+    xhsvd2s((J[0] + J[1]), A, U, Z, info);
 }
 
 #ifdef USE_EXTENDED
@@ -148,8 +232,10 @@ void zhsvd2_(dcomplex A[static 2][2], const fint J[static 2], dcomplex U[static 
   }
 
   xcomplex A_[2][2] = { { A[0][0], A[0][1] }, { A[1][0], A[1][1] } };
-  xcomplex U_[2][2] = { { CMPLXL(1.0L, 0.0L), CMPLXL(0.0L, 0.0L) }, { CMPLXL(0.0L, 0.0L), CMPLXL(1.0L, 0.0L) } };
-  xcomplex Z_[2][2] = { { CMPLXL(1.0L, 0.0L), CMPLXL(0.0L, 0.0L) }, { CMPLXL(0.0L, 0.0L), CMPLXL(1.0L, 0.0L) } };
+  xcomplex U_[2][2] = { { x1, x0 }, { x0, x1 } };
+  xcomplex Z_[2][2] = { { x1, x0 }, { x0, x1 } };
+
+  xhsvd2_(A_, J, U_, Z_, info);
 
   if (*info >= FINT_C(0)) {
     zassgn2(A, (const xcomplex (*)[2])A_);
