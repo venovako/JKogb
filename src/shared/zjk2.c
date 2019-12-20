df18509d8cc83a86eec0544f4fba3b6d217d2fa4
Vedran Novakovic <venovako@venovako.eu>
2019-12-20 19:31:55 +0100

    further changes

--- src/shared/zjk2.c
+++ src/shared/zjk2.c
@@ -39,10 +39,95 @@ static inline xcomplex xxdiv(const xcomplex a, const xcomplex b)
 #endif /* ?NDEBUG */
 }
 
+static inline void xswp(xcomplex a[static 1], xcomplex b[static 1])
+{
+  const xcomplex t = *a;
+  *a = *b;
+  *b = t;
+}
+
+static inline void xassgn1(xcomplex A[static 2], const xcomplex B[static 2])
+{
+  A[0] = B[0];
+  A[1] = B[1];
+}
+
+static inline void xassgn2(xcomplex A[static 2][2], const xcomplex B[static 2][2])
+{
+  A[0][0] = B[0][0];
+  A[0][1] = B[0][1];
+  A[1][0] = B[1][0];
+  A[1][1] = B[1][1];
+}
+
+static inline void zassgn2(dcomplex A[static 2][2], const dcomplex B[static 2][2])
+{
+  A[0][0] = (dcomplex)(B[0][0]);
+  A[0][1] = (dcomplex)(B[0][1]);
+  A[1][0] = (dcomplex)(B[1][0]);
+  A[1][1] = (dcomplex)(B[1][1]);
+}
+
+static inline void CA1(const xcomplex B[static 2][2], xcomplex A[static 2])
+{
+  const xcomplex C[2] =
+    {
+     (xfma(B[1][0], A[1], A[0]) / creall(B[0][0])),
+     (xfma(B[0][1], A[0], A[1]) / creall(B[1][1]))
+    };
+  xassgn1(A, C);
+}
+
+static inline void CA2(const xcomplex B[static 2][2], xcomplex A[static 2][2])
+{
+  const xcomplex C[2][2] =
+    {
+     {
+      (xfma(B[1][0], A[0][1], A[0][0]) / creall(B[0][0])),
+      (xfma(B[0][1], A[0][0], A[0][1]) / creall(B[1][1]))
+     },
+     {
+      (xfma(B[1][0], A[1][1], A[1][0]) / creall(B[0][0])),
+      (xfma(B[0][1], A[1][0], A[1][1]) / creall(B[1][1]))
+     }
+    };
+  xassgn2(A, C);
+}
+
+static inline void AC(xcomplex A[static 2][2], const xcomplex B[static 2][2])
+{
+  const xcomplex C[2][2] =
+    {
+     {
+      (xfma(A[1][0], B[0][1], A[0][0]) / creall(B[0][0])),
+      (xfma(A[1][1], B[0][1], A[0][1]) / creall(B[0][0]))
+     },
+     {
+      (xfma(A[0][0], B[1][0], A[1][0]) / creall(B[1][1])),
+      (xfma(A[0][1], B[1][0], A[1][1]) / creall(B[1][1]))
+     }
+    };
+  xassgn2(A, C);
+}
+
+static inline int zisfinite(const dcomplex z)
+{
+  return (isfinite(creal(z)) && isfinite(cimag(z)));
+}
+
 #ifdef USE_EXTENDED
 void zhsvd2_(dcomplex A[static 2][2], const fint J[static 2], dcomplex U[static 2][2], dcomplex Z[static 2][2], fint info[static 1])
 {
-  *info = FINT_C(0);
+  if (!zisfinite(A[0][0]))
+    *info = FINT_C(-1);
+  else if (!zisfinite(A[0][1]))
+    *info = FINT_C(-2);
+  else if (!zisfinite(A[1][0]))
+    *info = FINT_C(-3);
+  else if (!zisfinite(A[1][1]))
+    *info = FINT_C(-4);
+  else /* A OK */
+    *info = FINT_C(0);
 
   switch (J[0]) {
   case FINT_C(-1):
@@ -65,5 +150,11 @@ void zhsvd2_(dcomplex A[static 2][2], const fint J[static 2], dcomplex U[static 
   xcomplex A_[2][2] = { { A[0][0], A[0][1] }, { A[1][0], A[1][1] } };
   xcomplex U_[2][2] = { { CMPLXL(1.0L, 0.0L), CMPLXL(0.0L, 0.0L) }, { CMPLXL(0.0L, 0.0L), CMPLXL(1.0L, 0.0L) } };
   xcomplex Z_[2][2] = { { CMPLXL(1.0L, 0.0L), CMPLXL(0.0L, 0.0L) }, { CMPLXL(0.0L, 0.0L), CMPLXL(1.0L, 0.0L) } };
+
+  if (*info >= FINT_C(0)) {
+    zassgn2(A, (const xcomplex (*)[2])A_);
+    zassgn2(U, (const xcomplex (*)[2])U_);
+    zassgn2(Z, (const xcomplex (*)[2])Z_);
+  }
 }
 #endif /* USE_EXTENDED */
