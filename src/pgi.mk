a7cd3d9b4fa9d3572e4741aeccc9dfa3d645a7b0
Vedran Novakovic <venovako@venovako.eu>
2020-01-06 16:25:51 +0100

    Improve compatibility with the PGI compilers.

--- src/pgi.mk
+++ src/pgi.mk
@@ -1,8 +1,8 @@
 SHELL=/bin/bash
 ARCH=$(shell uname)
-ifeq ($(ARCH),Darwin)
-$(error PGI on macOS is not supported)
-endif # Darwin
+ifneq ($(ARCH),Linux)
+$(error PGI is supported on Linux only)
+endif # !Linux
 ifndef WP
 WP=8
 endif # !WP
@@ -15,6 +15,10 @@ RM=rm -rfv
 AR=ar
 ARFLAGS=rsv
 CPUFLAGS=-DUSE_PGI -DUSE_X64 -DUSE_TSC -DOLD_OMP -m64 -mp -KPIC -Mframe -Meh_frame -Minfo -Mnollvm
+ifdef PROFILE
+CPUFLAGS += -DVN_PROFILE=$(PROFILE) -Mnoinline -Minstrument=functions
+endif # PROFILE
+CPUFLAGS += -DTSC_FREQ_HZ=$(shell if [ `if [ -r /etc/redhat-release ]; then grep -c 'release 7' /etc/redhat-release; else echo 0; fi` = 1 ]; then echo `dmesg | grep 'TSC clocksource calibration' | cut -d':' -f3 | cut -d' ' -f2 | sed 's/\.//g'`000; else echo 0; fi)ull
 FORFLAGS=$(CPUFLAGS) -i8 -Mdclchk -Mlarge_arrays -Mrecursive -Mstack_arrays
 C11FLAGS=$(CPUFLAGS) -c11
 CC=pgcc
@@ -40,10 +44,7 @@ DBGCFLAGS=$(DBGFLAGS)
 FPUFFLAGS=$(FPUFLAGS)
 FPUCFLAGS=$(FPUFLAGS)
 endif # ?NDEBUG
-LIBFLAGS=-I. -I../shared -I../../../JACSD/vn
-ifeq ($(ARCH),Linux)
-LIBFLAGS += -D_GNU_SOURCE
-endif # Linux
-LDFLAGS=-L../shared -ljk$(DEBUG) -L../../../JACSD -lvn$(DEBUG) -pgf90libs -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
+LIBFLAGS=-D_GNU_SOURCE -I. -I../shared -I../../../JACSD/vn
+LDFLAGS=-L../shared -ljk$(PROFILE)$(DEBUG) -L../../../JACSD -lvn$(PROFILE)$(DEBUG) -pgf90libs -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
 FFLAGS=$(OPTFFLAGS) $(DBGFFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFFLAGS)
 CFLAGS=$(OPTCFLAGS) $(DBGCFLAGS) $(LIBFLAGS) $(C11FLAGS) $(FPUCFLAGS)
