b469e07e0f95ac38f5eee239bd93e86db06c4d0c
Vedran Novakovic <venovako@venovako.eu>
2020-09-03 21:51:20 +0200

    Update tweight.

--- src/alt/tweight.c
+++ src/alt/tweight.c
@@ -1,46 +1,52 @@
 #include "tweight.h"
 
-long double dtw(const double Aqp, const double Apq)
+long double dfn2(const double a, const double b)
 {
-  const double Aqp_ = fabs(Aqp);
-  const double Apq_ = fabs(Apq);
-  double M, m;
-  if (Aqp_ <= Apq_) {
-    M = Apq_;
-    m = Aqp_;
-  }
+  if (a == 0.0)
+    return ((b == 0.0) ? 0.0L : (b * (long double)b));
+  else if (b == 0.0)
+    return (a * (long double)a);
   else {
-    M = Aqp_;
-    m = Apq_;
+    const double a_ = fabs(a);
+    const double b_ = fabs(b);
+    double M, m;
+    if (a_ <= b_) {
+      M = b_;
+      m = a_;
+    }
+    else {
+      M = a_;
+      m = b_;
+    }
+    return fmal(M, M, m * (long double)m);
   }
-  return ((M == 0.0) ? qNaN() : fmal(M, M, m * (long double)m));
 }
 
-long double ztw(const double _Complex Aqp, const double _Complex Apq)
+long double zfn2(const double _Complex a, const double _Complex b)
 {
-  const double Aqp_i = fabs(cimag(Aqp));
-  const double Apq_i = fabs(cimag(Apq));
-  if ((Aqp_i == 0.0) && (Apq_i == 0.0))
-    return dtw(creal(Aqp), creal(Apq));
+  const double a_i = fabs(cimag(a));
+  const double b_i = fabs(cimag(b));
+  if ((a_i == 0.0) && (b_i == 0.0))
+    return dfn2(creal(a), creal(b));
   else { // at least one non-zero imaginary part
-    const double Aqp_r = fabs(creal(Aqp));
-    const double Apq_r = fabs(creal(Apq));
+    const double a_r = fabs(creal(a));
+    const double b_r = fabs(creal(b));
     double M_r, m_r, M_i, m_i, t;
-    if (Aqp_r <= Apq_r) {
-      M_r = Apq_r;
-      m_r = Aqp_r;
+    if (a_r <= b_r) {
+      M_r = b_r;
+      m_r = a_r;
     }
     else {
-      M_r = Aqp_r;
-      m_r = Apq_r;
+      M_r = a_r;
+      m_r = b_r;
     }
-    if (Aqp_i <= Apq_i) {
-      M_i = Apq_i;
-      m_i = Aqp_i;
+    if (a_i <= b_i) {
+      M_i = b_i;
+      m_i = a_i;
     }
     else {
-      M_i = Aqp_i;
-      m_i = Apq_i;
+      M_i = a_i;
+      m_i = b_i;
     }
     if (M_r < M_i) {
       t = M_r;
@@ -62,7 +68,19 @@ long double ztw(const double _Complex Aqp, const double _Complex Apq)
         m_i = t;
       }
     }
-    // now, M_r >= M_i >= m_r >= m_i
-    return ((M_r == 0.0) ? qNaN() : fmal(M_r, M_r, fmal(M_i, M_i, fmal(m_r, m_r, m_i * (long double)m_i))));
+    // now, M_r >= M_i >= m_r >= m_i > 0
+    return fmal(M_r, M_r, fmal(M_i, M_i, fmal(m_r, m_r, m_i * (long double)m_i)));
   }
+}
+
+long double dtw(const double Aqp, const double Apq)
+{
+  const long double w = dfn2(Aqp, Apq);
+  return ((w == 0.0L) ? qNaN() : w);
+}
+
+long double ztw(const double _Complex Aqp, const double _Complex Apq)
+{
+  const long double w = zfn2(Aqp, Apq);
+  return ((w == 0.0L) ? qNaN() : w);
 }
--- src/alt/tweight.h
+++ src/alt/tweight.h
@@ -2,6 +2,9 @@
 #define TWEIGHT_H
 #include "common.h"
 
+extern long double dfn2(const double a, const double b);
+extern long double zfn2(const double _Complex a, const double _Complex b);
+
 extern long double dtw(const double Aqp, const double Apq);
 extern long double ztw(const double _Complex Aqp, const double _Complex Apq);
 #endif /* !TWEIGHT_H */
