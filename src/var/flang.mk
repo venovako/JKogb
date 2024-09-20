2e773dd28a0e42434ddf7a4d879fa48ed4dcf721
Vedran Novakovic <venovako@venovako.eu>
2024-09-20 23:25:03 +0200

    further updates

---
+++ src/var/flang.mk
@@ -0,0 +1,34 @@
+SHELL=/bin/bash
+ARCH=$(shell uname)
+ifdef NDEBUG
+DEBUG=
+else # DEBUG
+DEBUG=g
+endif # ?NDEBUG
+RM=rm -rfv
+AR=ar
+ARFLAGS=rsv
+# comment out USE_X64 if not on Intel 64
+ifndef MARCH
+MARCH=native
+endif # !MARCH
+CPUFLAGS=-DUSE_FLANG -DUSE_X64 -fPIC -fimplicit-none -fno-omit-frame-pointer -march=$(MARCH) #-fopenmp
+ifeq ($(ARCH),Darwin)
+CPUFLAGS += -fintegrated-as
+endif # Darwin
+FORFLAGS=-cpp -DMIND=C_FMIN -DMAXD=C_FMAX $(CPUFLAGS) -fstack-arrays
+ifeq ($(ARCH),Darwin)
+CPUFLAGS += -DUSE_MACOS
+endif # Darwin
+FPUFLAGS=-ffp-contract=fast -fhonor-infinities -fhonor-nans
+FC=flang-new
+ifdef NDEBUG
+OPTFLAGS=-O$(NDEBUG) #-DUSE_FAST
+DBGFLAGS=-DNDEBUG
+else # DEBUG
+OPTFLAGS=-O$(DEBUG)
+DBGFLAGS=-$(DEBUG)
+endif # ?NDEBUG
+LIBFLAGS=-I. -I../shared
+LDFLAGS=-pthread -rdynamic -L. -l$(TYPE)jk$(DEBUG) -L../shared -ljk$(DEBUG)
+FFLAGS=$(OPTFLAGS) $(DBGFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFLAGS)
