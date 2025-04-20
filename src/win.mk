b70dfeb8f43687a87b144551a2d33b49a9544cbe
Vedran Novakovic <venovako@venovako.eu>
2025-04-20 21:33:25 +0200

    further updates

--- src/win.mk
+++ src/win.mk
@@ -1,26 +1,12 @@
 RM=del /F
 AR=lib.exe
 ARFLAGS=/NOLOGO /VERBOSE
-FORT=ifx
-FC=$(FORT).exe
-CPUFLAGS=/DUSE_INTEL /DUSE_X64 /DUSE_WINDOWS /Qopenmp
-FORFLAGS=/nologo /fpp $(CPUFLAGS) /standard-semantics
+FC=ifx.exe
 !IFNDEF MARCH
 MARCH=Host
 !ENDIF # !MARCH
-OPTFLAGS=/Qx$(MARCH)
-FPUFLAGS=/fp:precise /Qprotect-parens /Qfma /Qftz-
-LIBFLAGS=-I. -I..\shared /libs:dll /threads
-LDFLAGS=/link
-!IFDEF NDEBUG
-OPTFLAGS=/O$(NDEBUG) $(OPTFLAGS) /Qvec-threshold:0 #/DUSE_FAST
-DBGFLAGS=/DNDEBUG
-OPTFLAGS=$(OPTFLAGS) /Qopt-report:3
-LDFLAGS=$(LDFLAGS) /RELEASE /LIBPATH:. $(TYPE)jk.lib /LIBPATH:..\shared jk.lib
-!ELSE # DEBUG
-OPTFLAGS=/O$(DEBUG) $(OPTFLAGS)
-DBGFLAGS=/debug:full /debug:inline-debug-info /debug-parameters:all /check:all /warn:all
-LIBFLAGS=$(LIBFLAGS) /dbglibs
-LDFLAGS=$(LDFLAGS) /DEBUG /LIBPATH:. $(TYPE)jk$(DEBUG).lib /LIBPATH:..\shared jk$(DEBUG).lib
-!ENDIF # ?NDEBUG
-FFLAGS=$(OPTFLAGS) $(DBGFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFLAGS)
+!IFNDEF NDEBUG
+NDEBUG=d
+!ENDIF # NDEBUG
+FFLAGS=/nologo /fpp /Qopenmp /standard-semantics /traceback /DNDEBUG=$(NDEBUG) /DUSE_INTEL /DUSE_X64 /DUSE_WINDOWS /I. -I..\shared /O$(NDEBUG) /Qx$(MARCH) /fp:precise /Qfma /Qftz- /Qprec-div /Qprotect-parens /Qopt-report:3 /Qvec-threshold:0
+LDFLAGS=/link /RELEASE /LIBPATH:. $(TYPE)jk.lib /LIBPATH:..\shared jk.lib
