9e404189a83d34f3a790383c41f49a3f733a5268
Vedran Novakovic <venovako@venovako.eu>
2025-04-21 06:45:48 +0200

    further updates

--- src/win.mk
+++ src/win.mk
@@ -7,6 +7,6 @@ MARCH=Host
 !ENDIF # !MARCH
 !IFNDEF NDEBUG
 NDEBUG=d
-!ENDIF # NDEBUG
-FFLAGS=/nologo /fpp /Qopenmp /standard-semantics /traceback /DNDEBUG=$(NDEBUG) /DUSE_INTEL /DUSE_X64 /DUSE_WINDOWS /I. -I..\shared /O$(NDEBUG) /Qx$(MARCH) /fp:precise /Qfma /Qftz- /Qprec-div /Qprotect-parens /Qopt-report:3 /Qvec-threshold:0
+!ENDIF # !NDEBUG
+FFLAGS=/nologo /fpp /Qopenmp /standard-semantics /traceback /DNDEBUG=$(NDEBUG) /DUSE_INTEL /DUSE_X64 /DUSE_WINDOWS /I. -I..\shared /MD /O$(NDEBUG) /Qx$(MARCH) /fp:precise /Qfma /Qftz- /Qprec-div /Qprotect-parens /Qopt-report:3 /Qvec-threshold:0
 LDFLAGS=/link /RELEASE /LIBPATH:. $(TYPE)jk.lib /LIBPATH:..\shared jk.lib
